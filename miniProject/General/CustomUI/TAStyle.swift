bdf48e36d875dab51e67b87684b7f0f13ac9f15a
Min <qwer810520@gmail.com>
2018-07-19 17:25:52 +0800

    update Color

--- miniProject/General/CustomUI/TAStyle.swift
+++ miniProject/General/CustomUI/TAStyle.swift
@@ -12,5 +12,11 @@ class TAStyle {
     
     typealias JSONDictionary = [String: AnyObject]
     
+    class var orange: UIColor {
+        return #colorLiteral(red: 0.9987706218, green: 0.5847493011, blue: 0.007274393888, alpha: 1)
+    }
     
+    class var backgroundColor: UIColor {
+        return #colorLiteral(red: 1, green: 0.8980392157, blue: 0.7058823529, alpha: 1)
+    }
 }
---
+++ miniProject/General/Tabbar/TATabbarController.swift
@@ -0,0 +1,26 @@
+//
+//  TATabbarController.swift
+//  miniProject
+//
+//  Created by 楷岷 張 on 2018/7/19.
+//  Copyright © 2018年 楷岷 張. All rights reserved.
+//
+
+import UIKit
+
+class TATabbarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    // MARK: - private Method
+    
+    private func setUserInterface() {
+        
+    }
+}
